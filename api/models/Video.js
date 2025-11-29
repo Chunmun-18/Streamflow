f57479cab8179a846986d5689b861288ece9c3b3
Chunmun-18 <chunmungadodia@gmail.com>
2025-11-29 16:46:20 +0530

    Video upload issue resolved

---
+++ api/models/Video.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const videoSchema= new mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    title:{type:String, required:true},
+    description:{type:String, required:true},
+    user_id:{type:String, required:true},
+    videoUrl:{type:String, required:true},
+    videoId:{type:String, required:true},
+    thumbnailUrl:{type:String, required:true},
+    thumbnailId:{type:String, required:true},
+    category:{type:String, required:true},
+    tags:[{type:String}],
+    likes:{type:Number, default:0},
+    dislike:{type:Number, default:0},
+    likedby:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
+    dislikedby:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
+    viewedby:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}]
+},{timestamps:true}) //writing timestamps:true so that mongoose can itself manage the time at which it was created and last modified
+
+module.exports= mongoose.model("Video",videoSchema)
